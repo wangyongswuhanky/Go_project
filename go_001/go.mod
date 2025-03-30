775563fc2bbfcc76d2006846a96fe7e6323f2f59
wangyongswuhanky <1070051522@qq.com>
2025-03-30 14:14:28 +0800

    Initial commit

---
+++ go_001/go.mod
@@ -0,0 +1,3 @@
+module github.com/wanggyongswuhanky/demo
+
+go 1.24.1
---
+++ go_001/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello,Wangyong!!!")
+	fmt.Println("Hello,Wangyong!!!")
+	fmt.Println("Hello,Wangyong!!!")
+	fmt.Println("Hello,Wangyong!!!")
+	fmt.Println("Hello,Wangyong!!!")
+	fmt.Println("Hello,Wangyong!!!")
+	fmt.Println("Hello,Wangyong!!!")
+	fmt.Println("Hello,Wangyong!!!")
+	fmt.Println("Hello,Wangyong!!!")
+	fmt.Println("Hello,Wangyong!!!")
+	fmt.Println("Hello,Wangyong!!!")
+	fmt.Println("Hello,Wangyong!!!")
+	fmt.Println("Hello,Wangyong!!!")
+	fmt.Println("Hello,Wangyong!!!")
+	fmt.Println("Hello,Wangyong!!!")
+	fmt.Println("Hello,Wangyong!!!")
+	fmt.Println("Hello,Wangyong!!!")
+	fmt.Println("Hello,Wangyong!!!")
+	fmt.Scanln()
+}
